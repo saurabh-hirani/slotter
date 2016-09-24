9396b33dc10d138393e22a54f20394d8b95b945d
Saurabh Hirani <saurabh.hirani@gmail.com>
2016-09-25 01:14:35 +0530

    syncing up with readme sample code

--- sample.py
+++ sample.py
@@ -16,18 +16,18 @@ s.add_item(11)
 s.add_item(15)
 
 print "Has 5 been slotted?"
-print s.slotted(5)   # has 5 been added
+print s.slotted(5)
 print "=================================="
 
 print "Target slot of 5"
-target_slot = s.find_slot(5) # in which slot has 5 been added
+target_slot = s.find_slot(5)
 print "Target slot start - %s" % str(target_slot.start)
 print "Target slot end - %s" % str(target_slot.end)
 print "Target slot str representation - %s" % str(target_slot)
 print "=================================="
 
 print "Target slot of 11"
-target_slot = s.find_slot(11) # in which slot has 5 been added
+target_slot = s.find_slot(11)
 print "Is 15 in the same target slot?"
 print 15 in target_slot
 print "=================================="
@@ -41,15 +41,15 @@ print s.items
 print "=================================="
 
 print "All slot items"
-print s.slot_items # return dictionary of slot -> items
+print s.slot_items # slot -> items dictionary
 print "=================================="
 
 print "All item slots"
-print s.item_slots # return dictionary of item -> slots
+print s.item_slots # item -> slots dictionary
 print "=================================="
 
 print "Dumpable ds"
-print json.dumps(s.dump(), indent=2) # return a json dumpable version of the ds
+print json.dumps(s.dump(), indent=2)
 print "=================================="
 
 print "Dumpable ds - reverse"
